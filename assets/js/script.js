5f4eb14f6d3024457da4e6c13694ed7d84f8dce0
ColeW <HashBrownBrother024@gmail.com>
2023-11-21 17:20:20 -0500

    added the last bit of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,5 +32,33 @@ for(var i = 0; i < pswdLength; i++) {
 return password;
 }
 
+function getPrompts() {
+  pswdArray = [];
+
+  pswdLength = parseInt (prompt("How many characters do you want the password to be?"));
+
+  if(isNaN(pswdLength) || pswdLength <8 || pswdLength > 128) {
+    alert("Character length has to be a number, 8 - 128 digits. Please try again.");
+    return false;
+  }
+
+  if (confirm("Do you want lowercase letters in your password?")) {
+    pswdArray = pswdArray.concat(lowerCaseLtrArray);
+  }
+
+  if (confirm("Do you want uppercase letters in your password?")) {
+    pswdArray = pswdArray.concat(upperCaseLtrArray);
+  }
+
+  if (confirm("Do you want special characters in your password?")) {
+    pswdArray = pswdArray.concat(spCharArray);
+  }
+
+  if (confirm("Do you want numbers in your password?")) {
+    pswdArray = pswdArray.concat(numberArray);
+  }
+  return true;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
