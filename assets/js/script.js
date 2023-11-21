fda9b6f85fed5d9b57bd251dfdebfc87b1c905df
ColeW <HashBrownBrother024@gmail.com>
2023-11-21 18:24:38 -0500

    fixed the link to the js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var pswdLength  = 8;
-var pswdArray =[];
+var pswdLength = 8;
+var pswdArray = [];
 var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
 var lowerCaseLtrArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCaseLtrArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -9,22 +9,21 @@ var spCharArray = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var correctPrompts = getPrompts(); //true or false
   var passwordText = document.querySelector("#password");
 
-  if(correctPrompts) {
-  var randomPassword = generatePassword();
-  passwordText.value = randomPassword;
-} else {
+  if(corectPrompts) {
+    var randomPassword = generatePassword()
+    passwordText.value = randomPassword;
+  } else{
   passwordText.value = "";
 }
 
 }
 
 function generatePassword() {
-// generate password based on user input
 var password ="";
 for(var i = 0; i < pswdLength; i++) {
   var randomIndex = Math.floor(Math.random() * pswdArray.length);
