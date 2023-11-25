5009411ce0994f1219186d5490fa83ea7deb6bdd
ColeW <HashBrownBrother024@gmail.com>
2023-11-25 12:01:09 -0500

    reworked javascript, moved the array for special characters

--- script.js
+++ script.js
@@ -13,7 +13,16 @@ function writePassword() {
 
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
 
+var pswdLength = 8;
+var pswdArray = [];
+var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
+var lowerCaseLtrArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCaseLtrArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var spCharArray = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
+"~", "*", "?", ">", "<", "@", "#", "$", "%"];
 
 function getPrompts() {
   pswdArray = [];
@@ -42,6 +51,9 @@ function getPrompts() {
   }
   return true;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+console.log(passArray);
+var word ="";
+console.log(lengths)
+for (let index = 0; index < lengths; index++) {
+      word += passArray [Math.floor(Math.random() * passArray.length)];
+}
