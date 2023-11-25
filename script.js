e61b6bb164eac1af514930a82b3fea18e28342fe
ColeW <HashBrownBrother024@gmail.com>
2023-11-25 15:53:04 -0500

    complete overhaul, rewrote js file

--- script.js
+++ script.js
@@ -1,72 +1,130 @@
-
 // Assignment Code
+
+// global variables
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var spec = ["!", "@", "#", "$", "%", "&", "*", "+"]
+var randomChar = []; 
+
+
+// generate password function
+var generatePassword = function () {
+
+  // character length prompt
+  var charLength = prompt('Please enter length of password; enter a number between "8" and "128".');
+
+  if (charLength < 8 || charLength > 128) {
+    alert('Must be a number between "8" and "128"');
+    return generatePassword();
+  } else if (!charLength) {
+    alert('A number value is needed before proceeding')
+    return generatePassword();
+  }
+
+
+// confirm option for lowercase characters
+  var charLower = confirm('Would you like to use lowercase characters?')
+
+  if (!charLower) {
+    charLower = '';
+  } else {
+    charLower = lower;
+  }
+ 
+
+// confirm option for uppercase characters
+  var charUpper = confirm('Would you like to use uppercase characters?')
+
+  if (!charUpper) {
+    charUpper = '';
+  } else {
+    charUpper = upper;
+  }
+
+
+// confirm option for number characters
+  var charNum = confirm('Would you like to use number characters?')
+
+  if (!charNum) {
+    charNum = '';
+  } else {
+    charNum = num
+  }
+
+  
+// confirm option for special charcters
+  var charSpec = confirm('Would you like to use special characters?')
+
+  if (!charSpec) {
+    charSpec = '';
+  } else {
+    charSpec = spec;
+  }
+
+  
+
+// requires that user meet perameters
+  var totalChar = charLower + charUpper + charNum + charSpec;
+
+  if (totalChar.length <= 0) {
+    alert('Atleast one type must be chosen');
+    return generatePassword();
+  }
+
+ 
+
+// gives randomChar a value for loop
+  if (charLower) {
+    randomChar = randomChar.concat(lower);
+  } else if (!charLower) {
+    charLower = '';
+  }
+
+
+  if (charUpper) {
+    randomChar = randomChar.concat(upper);
+  } else if (!charUpper) {
+    charUpper = '';
+  }
+
+
+  if (charNum) {
+    randomChar = randomChar.concat(num);
+  } else {
+    num = '';
+  }
+
+
+  if (charSpec) {
+    randomChar = randomChar.concat(spec);
+  } else {
+    spec = '';
+  }
+
+
+
+// randomize charLength with added perameters
+  var finalPassword = []
+  for (i = 0; i < charLength; i++) {
+    var range =[Math.floor(Math.random() * randomChar.length)];
+    finalPassword = finalPassword + randomChar[range];
+  }
+
+  return finalPassword;
+
+}
+
+// apply .js to id's
 var generateBtn = document.querySelector("#generate");
 
-
 function writePassword() {
-
   var password = generatePassword();
-
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); 
-
-var pswdLength = 8;
-var pswdArray = [];
-var numberArray = ['0','1','2','3','4','5','6','7','8','9'];
-var lowerCaseLtrArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-var upperCaseLtrArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var spCharArray = ["+", "-", "&", "|", "!", "(", ")", "{", "}", "[", "]", "^",
-"~", "*", "?", ">", "<", "@", "#", "$", "%"];
-
-// My code
-var lengths = prompt("How many characters would you like the password to contain");
-console.log(length)
-if (!lengths || isNaN(lengths) == true ) {
-  alert("please enter a numeric value");
-  generatePassword()
-}
-else if (lengths < 8 || lengths >128) {
-  alert("Password length must be between 8 and 128 characters!");
-  generatePassword()
-}
-else {
-
-var useSpecial = confirm("Do you want special characters?");
-
-var useNumb = confirm( "Do you want numbers?");
-
-var useLower = confirm( "Do you want lowercase letters?");
-
-var useUpper = confirm("Do you waant uppercase letters?");
-
-
-//Password Options Array
-var passArray = []
-
-if (useSpecial) {
-  passArray = passArray.concat(specialChars);
-}
-if (useNumb) {
-  passArray = passArray.concat(numbersChars);
-}
-if (useLower) {
-  passArray = passArray.concat(uppercase)
-}
-console.log(passArray);
-var word ="";
-console.log(lengths)
-for (let index =0; index < lengths; index++) {
-
-  //random values from passArray
-  word +=passArray[Math.floor(Math.random() * passArray.length)];
-
-}
-return word;
-}
- 
+// event listener to generate button
+generateBtn.addEventListener("click", writePassword);
